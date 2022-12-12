c752a05c11d69f69bd72e66a1ae719765bd3c9eb
Bleuyogurt <wilsonodoemelam@gmail.com>
2022-12-12 13:59:31 -0800

    Add library for lib and time

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - A program that tells negative, possitive and zero numbers
+ *
+ * Return (0) on success
+ */
+int main(void)
+{	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This project is all on the use of variables, if, if..else statements to tackle problems in C
