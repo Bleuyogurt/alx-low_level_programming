cb7acd86b9118b5e0218be6dd6375edc23918414
Bleuyogurt <wilsonodoemelam@gmail.com>
2022-12-12 17:15:44 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the character for hexadecimal
+ * Return: Returns 0 on successful completion of program
+ */
+int main(void)
+{
+	int i;
+	char ch;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
