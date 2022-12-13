ea576ecd0c568da41bbbf7150834d1959e6c202b
Bleuyogurt <wilsonodoemelam@gmail.com>
2022-12-12 17:04:26 -0800

    Printing alphabets in decending manner

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints alphabet a -z in reverse then a new line
+ * Return: Returns 0 on successful completion of program
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'z';
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
