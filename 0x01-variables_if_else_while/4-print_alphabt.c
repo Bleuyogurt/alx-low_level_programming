5e4e44f848f8232f4b4f181efbd32dcb264b6d65
Bleuyogurt <wilsonodoemelam@gmail.com>
2022-12-12 16:14:48 -0800

    Taask 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all aphabet from a to z except e and q
+ *
+ * Return: Returns 0 on successful completion of program
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
