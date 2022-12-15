af063b9ae6a6ecef1d683e936176a7aa9b2aceb4
Bleuyogurt <wilsonodoemelam@gmail.com>
2022-12-15 15:22:20 -0800

    program that multiplies 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: return product of both integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
