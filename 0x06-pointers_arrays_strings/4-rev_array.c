477a6171a18a483ae019a06e9e4def3d1eb74681
Bleuyogurt <wilsonodoemelam@gmail.com>
2023-01-03 16:45:22 -0800

    tasks

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverse the content on an array
+ * @a: pointer to the array
+ * @n: the number of elements to swap
+ *
+ * Return: no return value
+ */
+void reverse_array(int *a, int n)
+{
+	int i, c, temp;
+
+	i = n - 1;
+	c = 0;
+
+	while (c < i)
+	{
+		temp = a[c];
+		a[c] = a[i];
+		a[i] = temp;
+		c++;
+		i--;
+	}
+}
